1cac615ac1b1a4d42e8840430253dc58614ff565
luk9400 <lukaszbratos@gmail.com>
2019-03-06 01:03:05 +0100

    pfff

--- list1/task2/app/src/main/java/com/example/wsiobus/MainActivity.kt
+++ list1/task2/app/src/main/java/com/example/wsiobus/MainActivity.kt
@@ -2,11 +2,30 @@ package com.example.wsiobus
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.renderscript.ScriptGroup
+import android.support.v7.widget.AppCompatEditText
+import android.text.format.Time
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    val c845: List<Double> = listOf(4.43, 5.29, 8.15, 12.45, 14.45, 15.45, 19.05)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun search() {
+        var hour = findViewById<AppCompatEditText>(R.id.input).text
+        println(hour)
+        findViewById<TextView>(R.id.busOutput1).text = "845"
+        findViewById<TextView>(R.id.timeOutput1).text = c845.get(0).toString()
+        findViewById<TextView>(R.id.stopOutput1).text = "Cmentarz"
+    }
+
+    fun searchButton(view: View) {
+        search()
     }
 }
--- list1/task2/app/src/main/java/com/example/wsiobus/Timetables.kt
+++ list1/task2/app/src/main/java/com/example/wsiobus/Timetables.kt
@@ -1,5 +1,10 @@
 package com.example.wsiobus
 
 class Timetables {
-    val bus845: List<Double> = listOf(17.45)
+    val c845: List<Double> = listOf(4.43, 5.29, 8.15, 12.45, 14.45, 15.45, 19.05)
+
+    public fun get845(index: Int): Double {
+        return c845.get(index)
+    }
+
 }
