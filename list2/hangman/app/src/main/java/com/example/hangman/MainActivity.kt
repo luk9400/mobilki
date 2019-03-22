7c5ce2bea6242d59c7b8d12e396afc87a4846aa3
luk9400 <lukaszbratos@gmail.com>
2019-03-22 14:25:27 +0100

    win info and finishing touches

--- list2/hangman/app/src/main/java/com/example/hangman/MainActivity.kt
+++ list2/hangman/app/src/main/java/com/example/hangman/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.hangman
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,24 +14,33 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         game = Game(resources.getStringArray(R.array.words))
-        textView.text = (game as Game).censorWord((game as Game).word)
+        // game needs !! or 'as Game' :(
+        textView.text = game!!.censorWord((game as Game).word)
     }
 
     fun click(view: View) {
         if (inputText.text.toString() != "") {
             val char = inputText.text.first()
+            val game = game!!
 
-            if ((game as Game).word.contains(char, true)) {
-                (game as Game).checker((game as Game).word, char, 0)
-                textView.text = (game as Game).censorWord((game as Game).word)
+            if (game.word.contains(char, true)) {
+                game.checker(game.word, char, 0)
+                textView.text = game.censorWord(game.word)
+                if (textView.text.toString() == game.word && game.lives > 0) {
+                    Toast.makeText(this, "You won!", Toast.LENGTH_LONG).show()
+                }
             } else {
-                if ((game as Game).lives > 0) {
-                    (game as Game).lives--
-                    val stage = 13 - (game as Game).lives
+                if (game.lives > 0) {
+                    game.lives--
+                    val stage = 13 - game.lives
                     val id = resources.getIdentifier("hangman$stage", "drawable", applicationContext.packageName)
                     imageView.setImageResource(id)
+                    if (game.lives == 0) {
+                        Toast.makeText(this, "You lost!", Toast.LENGTH_LONG).show()
+                    }
                 }
             }
+            inputText.setText("")
         }
     }
 }
